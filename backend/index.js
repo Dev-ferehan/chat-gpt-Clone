294891d45a5bb0cf3ee835578b40b534917bc236
Dev-ferehan <ahmedferehan7@gmail.com>
2026-09-25 17:21:57 +0300

    fix: update server listening port and host binding for render

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(errorHandler);
 // 4. Server and Database Initialization
 async function startServer(){
   try {
-    app.listen(port, () => {
+    app.listen(port,"0.0.0.0", () => {
       console.log("Server is running on port ",port);
     });
   } catch (err) {
