c4790a4dcbb07bb0b40a6028a406bd911de3d3c2
Dev-ferehan <ahmedferehan7@gmail.com>
2026-09-25 17:03:57 +0300

    fix: update server listening port and host binding for render

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import mainRouter from "./src/main.routes.js";
 import { errorHandler } from "./src/middleware/error-handler.js";
 import db from "./db/db.config.js";
-
+const port = process.env.DB_PORT || 9000;
 
 const app = express();
 
@@ -23,8 +23,8 @@ app.use(errorHandler);
 // 4. Server and Database Initialization
 async function startServer(){
   try {
-    app.listen(process.env.DB_PORT, () => {
-      console.log("Server is running on port ", process.env.DB_PORT);
+    app.listen(port, () => {
+      console.log("Server is running on port ",port);
     });
   } catch (err) {
     console.error("Failed to start server:", err.message);
